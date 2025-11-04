09944129513088d157892bbc70cc9dd876e2448e
Saifur Rahman <saifurrshishir@gmail.com>
2025-11-04 10:43:14 +0600

    Problem 6 solved

---
+++ problem__6.py
@@ -0,0 +1,7 @@
+username = input("Enter Username: ")
+password = input("Enter Password: ")
+
+if username == 'admin' and password == '12345':
+    print("Login Successful")
+else:
+    print("Invalid Credentials")
